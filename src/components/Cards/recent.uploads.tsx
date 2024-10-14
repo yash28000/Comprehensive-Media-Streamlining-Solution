8ce7ba9ecee8594d7f7ba51a5cfc283d56ed1caf
Jatin Kumar <jatinkumar10082003@gmail.com>
2024-10-14 10:51:44 +0530

    changes in layout

--- src/components/Cards/recent.uploads.tsx
+++ src/components/Cards/recent.uploads.tsx
@@ -1,10 +1,13 @@
+"use client";
+
 import React, { useRef } from "react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
-import { Button } from "../ui/button";
+import { Button } from "@/components/ui/button";
 import { useAppSelector } from "@/reducers/store";
 import { IVideo } from "@/services/videos/videos.types";
 import Image from "next/image";
 import { getDateString } from "@/utils/functions";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 
 export const RecentUploads = () => {
   const { user } = useAppSelector((root) => root.user);
@@ -21,64 +24,65 @@ export const RecentUploads = () => {
   };
 
   return (
-    <div className="border-[1px] relative border-zinc-200 shadow-sm py-8 px-10 rounded-xl h-auto">
+    <div className="relative z-10 w-full h-auto py-8 px-4 lg:px-10 border border-zinc-200 rounded-xl shadow-sm overflow-hidden">
       <h1 className="text-lg font-bold">Recents</h1>
-      <div className="mt-10 overflow-hidden w-full">
-        <div
-          ref={scrollContainerRef}
-          className="flex gap-5 overflow-x-auto scrollbar-hide"
-          style={{ scrollSnapType: "x mandatory" }}
-        >
-          {user?.uploads[0] &&
-            user.uploads.map((video, index) => (
-              <VideoCard key={index} video={video} userAvatar={user.avatar} />
-            ))}
-        </div>
+      <div className="mt-10 w-full">
+        <ScrollArea className="w-full -z-10" ref={scrollContainerRef}>
+          <div className="flex space-x-4">
+            {user?.uploads[0] &&
+              user.uploads.map((video, index) => (
+                <VideoCard key={index} video={video} userAvatar={user.avatar} />
+              ))}
+          </div>
+          <ScrollBar orientation="horizontal" />
+        </ScrollArea>
       </div>
-      
-      <div className="flex gap-1 absolute right-5 top-5">
+
+      <div className="absolute right-5 top-5 flex gap-1">
         <Button
-          variant="link"
-          className="bg-zinc-100 p-0 size-8"
+          variant="outline"
+          size="icon"
+          className="hidden md:flex"
           onClick={() => scroll("left")}
         >
-          <ChevronLeft size={20} />
+          <ChevronLeft className="h-4 w-4" />
         </Button>
         <Button
-          variant="link"
-          className="bg-zinc-100 p-0 size-8"
+          variant="outline"
+          size="icon"
+          className="hidden md:flex"
           onClick={() => scroll("right")}
         >
-          <ChevronRight size={20} />
+          <ChevronRight className="h-4 w-4" />
         </Button>
       </div>
     </div>
   );
 };
 
-const VideoCard = ({
+function VideoCard({
   video,
   userAvatar,
 }: {
   video: IVideo;
   userAvatar: string;
-}) => {
+}) {
   return (
-    <div className="w-[300px] flex-shrink-0 scroll-snap-align-start">
-      <div className="relative w-[300px] h-[200px]">
+    <div className="w-[250px] sm:w-[300px] flex-shrink-0">
+      <div className="relative w-full h-[140px] sm:h-[180px]">
         <Image
           src={
-            video.thumbnail
-              ? video.thumbnail
-              : `${process.env.NEXT_PUBLIC_OUTPUT_BUCKET}/${video.videoFile}/thumbnail.jpg`
+            video.thumbnail ||
+            `${process.env.NEXT_PUBLIC_OUTPUT_BUCKET}/${video.videoFile}/thumbnail.jpg`
           }
           alt={video.title}
           layout="fill"
           objectFit="cover"
+          className="rounded-lg"
         />
       </div>
       <div className="flex mt-2 items-center space-x-2">
-        <div className="size-10 rounded-full border-[1px] relative z-10 border-zinc-200 flex items-center justify-center shadow-sm">
+        <div className="relative z-10 flex items-center justify-center w-10 h-10 border border-zinc-200 rounded-full shadow-sm">
           <Image
             src={userAvatar}
             alt="user"
@@ -96,4 +100,4 @@ const VideoCard = ({
       </div>
     </div>
   );
-};
+}
